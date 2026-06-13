fdbfbb7827fd433ce14a1c84c3ecea19b31ba252
Ern Qi Teng <ernqiteng@yahoo.com>
2026-06-14 00:37:14 +0100

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0){
+            return 1;
+        }
+
+        int power = 0;
+        if (n > 0){
+            power = n;
+        } else {
+            power = -n;
+        }
+
+        double num = x;
+        int count = 1;
+
+        while (count != power){
+            num = num*x;
+            count++;
+        }
+        if (n > 0){
+            return num;
+        }
+        return 1/num;
+    }
+};
