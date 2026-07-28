8a95b0b075f92227c61bfd77fa77ce886aed8d9a
Ern Qi Teng <ernqiteng@yahoo.com>
2026-07-28 23:51:32 +0100

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxprofit = {} #key = index, owns   value = maxprofit
+        def backtrack(index, owns):
+            if index >= len(prices):
+                return 0
+            
+            if (index, owns) in maxprofit:
+                return maxprofit[(index, owns)]
+            
+            #don't buy/sell
+            leave = backtrack(index+1, owns)
+            if not owns:
+                #buy
+                maxprofit[(index, owns)] = max(backtrack(index+1, True)-prices[index], leave)
+            else:
+                #sell 
+                maxprofit[(index, owns)] = max(backtrack(index+2, False)+prices[index], leave)
+            return maxprofit[(index, owns)]
+
+        return backtrack(0,False)
